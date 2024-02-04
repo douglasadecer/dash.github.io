e4750e166a8876fae24c1940d2df0e7ae210eb29
Douglas Adecer <douglasadecer@y7mail.com>
2024-02-05 08:22:09 +1100

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -30,7 +30,7 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           <Image
-        src="/hero-desktop.png"
+        src="./hero-desktop.png"
         width={1000}
         height={760}
         className="hidden md:block"
